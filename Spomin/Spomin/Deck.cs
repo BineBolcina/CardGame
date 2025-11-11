ad8b6fc58eadb28f8639cb10d1d3900e2e8c8a84
BineBolcina <151830242+BineBolcina@users.noreply.github.com>
2025-11-11 22:14:15 +0100

    Add files via upload

---
+++ Spomin/Spomin/Deck.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Spomin
+{
+    public class Deck
+    {
+        public List<Card> Cards { get; set; }
+
+        public Deck()
+        {
+            Cards = new List<Card>();
+            string[] names = { "A_Hearts", "2_Hearts", "3_Hearts", "4_Hearts",
+                               "5_Hearts", "6_Hearts", "7_Hearts", "8_Hearts" };
+
+            foreach (var n in names)
+            {
+                Cards.Add(new Card(n));
+                Cards.Add(new Card(n)); // par
+            }
+
+            Shuffle();
+        }
+
+        public void Shuffle()
+        {
+            Random rng = new Random();
+            Cards = Cards.OrderBy(a => rng.Next()).ToList();
+        }
+    }
+}
